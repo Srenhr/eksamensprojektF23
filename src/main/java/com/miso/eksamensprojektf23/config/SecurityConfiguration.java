ba401e3496371a7894b703a54af0e63aa227bf6a
SrenhrKEA <soer53k0@stud.kea.dk>
2023-05-04 11:48:33 +0200

    CHANGES: - disables security for all endpoints with pattern ("/**") to allow for testing

--- src/main/java/com/miso/eksamensprojektf23/config/SecurityConfiguration.java
+++ src/main/java/com/miso/eksamensprojektf23/config/SecurityConfiguration.java
@@ -3,14 +3,10 @@ package com.miso.eksamensprojektf23.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +24,7 @@ public class SecurityConfiguration {
         .csrf()
         .disable()
         .authorizeHttpRequests((authz) -> authz
-            .requestMatchers("/resources/**", "/static/**","/", "/home", "/about", "/error/**")
+            .requestMatchers("/resources/**", "/static/**","/", "/home", "/about", "/error/**","/**")
             .permitAll()
             .requestMatchers("/admin/**").hasRole("ADMIN")
             .requestMatchers("/user/**").hasRole("USER")
