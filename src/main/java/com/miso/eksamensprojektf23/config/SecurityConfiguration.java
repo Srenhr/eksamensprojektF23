498e6cf792082a578d676fb481085f7cf44046ca
CodeClod <mika9103@stud.kea.dk>
2023-05-15 11:39:08 +0200

    Stuff for testing calendarTest.html

--- src/main/java/com/miso/eksamensprojektf23/config/SecurityConfiguration.java
+++ src/main/java/com/miso/eksamensprojektf23/config/SecurityConfiguration.java
@@ -24,7 +24,7 @@ public class SecurityConfiguration {
         .csrf()
         .disable()
         .authorizeHttpRequests((authz) -> authz
-            .requestMatchers("/js/**", "/css/**", "/img/**", "/scss/**", "/", "/home", "/about", "/error/**"/*, "/**"*/, "/public/**")
+            .requestMatchers("/js/**", "/css/**", "/img/**", "/scss/**", "/", "/home", "/about", "/error/**", "/**", "/public/**")
             .permitAll()
             .requestMatchers("/auth/**").hasRole("ADMIN")
             .anyRequest()
