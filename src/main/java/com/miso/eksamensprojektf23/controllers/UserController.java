dd67af5ec887c94210644300259bbc492ed0e1cb
CodeClod <mika9103@stud.kea.dk>
2023-05-18 09:39:47 +0200

    Safety commit before moving Javascript from script tag to .js file

--- src/main/java/com/miso/eksamensprojektf23/controllers/UserController.java
+++ src/main/java/com/miso/eksamensprojektf23/controllers/UserController.java
@@ -6,12 +6,18 @@ import com.miso.eksamensprojektf23.models.User;
 import com.miso.eksamensprojektf23.repositories.AppointmentRepository;
 import com.miso.eksamensprojektf23.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -23,10 +29,23 @@ public class UserController {
     @Autowired
     AppointmentRepository appointmentRepository;
 
-    @GetMapping("/employees/{employeeId}/appointments")
+
+    // Used for testing purposes
+    @GetMapping("/users/1/appointments")
     public List<Appointment> employeeAppointments(@PathVariable long employeeId) {
         User employee = userRepository.findByUserId(employeeId);
         return appointmentRepository.findAppointmentsByUser(employee);
+    }
+
+    // Implement to fetch in calendar when using springboot login
+    @GetMapping("/user/userappointments")
+    public List<Appointment> getSomeData(Authentication authentication) {
+        String loggedInUsername = ((UserDetails) authentication.getPrincipal()).getUsername();
+        // Use the logged-in user's ID to fetch data specific to that user
+
+        Optional<User> loggedInUser = userRepository.findUserByUsername(loggedInUsername);
+
+        return appointmentRepository.findAppointmentsByUser(loggedInUser.get());
     }
 
 
--- src/main/resources/static/js/fullcalendar.js
+++ src/main/resources/static/js/fullcalendar.js
@@ -1,21 +1,27 @@
 import * as FullCalendar from "@fullcalendar/core";
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var calendarEl = document.getElementById('calendar');
 
-    // Fetch the appointments for the employee from the backend
-    fetch('/employees/1/appointments')
+    // Fetch appointments and initialize FullCalendar
+    fetch('http://localhost:8080/employees/1/appointments')
         .then(response => response.json())
         .then(data => {
             // Format the appointment data for FullCalendar
             var events = data.map(appointment => ({
-                id: appointment.id,
-                title: appointment.title,
+                id: appointment.appointmentId,
+                title: appointment.user.firstName + ' ' + appointment.user.lastName,
                 start: appointment.startTimeIso8601,
                 end: appointment.endTimeIso8601,
                 // Add any additional properties or formatting as needed
             }));
+
+            function formatTime(time) {
+                var date = new Date(time);
+                var hour = date.getHours();
+                var minute = date.getMinutes();
+                return ('0' + hour).slice(-2) + ':' + ('0' + minute).slice(-2);
+            }
 
             // Initialize FullCalendar
             var calendar = new FullCalendar.Calendar(calendarEl, {
@@ -26,13 +32,18 @@ document.addEventListener('DOMContentLoaded', function() {
                 events: events,
                 // Specify the event time format for display
                 eventTimeFormat: {
-                    hour: 'numeric',
+                    hour: '2-digit',
                     minute: '2-digit',
-                    meridiem: 'short'
+                    hour12: false,
+                    hour24: true
                 },
                 // Add event click handler
                 eventClick: function(info) {
-                    // Handle event click logic here
+                    var appointmentId = info.event.id
+
+                    // Potential redirect into the chosen appointment should be possible if client wants functionality.
+                    window.location.href = '/appointments/' + appointmentId;
+
                     console.log('Event clicked:', info.event);
                 }
             });
